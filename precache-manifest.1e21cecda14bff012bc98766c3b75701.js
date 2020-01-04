944f49b544cdc57482be4efc3a44396fe2d4acf6
yashverma2110 <yash.silver2110@gmail.com>
2020-01-04 18:48:34 +0530

    Updates

---
+++ precache-manifest.1e21cecda14bff012bc98766c3b75701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d85fd536b5e36fc8a11dc6eb9b9185",
+    "url": "/Speech-Emulator/index.html"
+  },
+  {
+    "revision": "f8aa5af9dae112c1b20f",
+    "url": "/Speech-Emulator/static/css/main.72f95aaf.chunk.css"
+  },
+  {
+    "revision": "0f089838bde54c365560",
+    "url": "/Speech-Emulator/static/js/2.00148e96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Speech-Emulator/static/js/2.00148e96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8aa5af9dae112c1b20f",
+    "url": "/Speech-Emulator/static/js/main.9209c3a0.chunk.js"
+  },
+  {
+    "revision": "cb8be7af28fc9dcbe00e",
+    "url": "/Speech-Emulator/static/js/runtime-main.667729b9.js"
+  },
+  {
+    "revision": "bd511318f94da2783891372c4452b964",
+    "url": "/Speech-Emulator/static/media/play-button-arrowhead.bd511318.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/Speech-Emulator/static/media/sort-down.ca82bc74.svg"
+  }
+]);
